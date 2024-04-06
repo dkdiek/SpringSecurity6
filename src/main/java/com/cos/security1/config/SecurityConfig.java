51e79247a63f29a6c775d31dd1dd68314980fdd9
Cliff <developerlyk@gmail.com>
2024-04-06 19:13:58 +0900

    커스텀 로그인

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+//        특정 경로에 대한 인가 작업
+        http
+                .authorizeHttpRequests((auth)-> auth
+                        .requestMatchers("/","/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN","USER")
+                        .anyRequest().authenticated()
+                );
+//        커스텀 로그인 페이지
+        http
+                .formLogin((auth)-> auth.loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll()
+                );
+//        csrf 개발 환경에서 잠시 삭제
+        http
+                .csrf((auth)->auth.disable());
+        return http.build();
+
+
+    }
+
+}
