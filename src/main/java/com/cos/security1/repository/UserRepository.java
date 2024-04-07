7ef1e51d678d951f6861321b9cf7adb0d16b8c33
Cliff <developerlyk@gmail.com>
2024-04-07 11:20:25 +0900

    회원중복검증 방법 jpa exist

--- src/main/java/com/cos/security1/repository/UserRepository.java
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+// wher절을 사용하여    username 존재하면 true 존재하지 않으면 false
+    boolean existsByUsername(String username);
 }
--- src/main/java/com/cos/security1/service/JoinService.java
+++ src/main/java/com/cos/security1/service/JoinService.java
@@ -17,6 +17,10 @@ public class JoinService {
 
     public void joinProcess(JoinDto joinDto){
         //db에 이미 동일한 username가진 회원이 존재하는지?
+        boolean isUser = userRepository.existsByUsername(joinDto.getUsername());
+        if(isUser){
+            return;
+        }
 
 
         UserEntity data = new UserEntity();
